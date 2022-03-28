3c229d2ad27a63a385948a71112839a61400d6c5
Amit Salaria <amit.salaria@payu.in>
2022-03-28 17:29:48 +0530

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -10,55 +10,50 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PayUIndia-Logger",
-            targets: ["PayUIndia-Logger"]),
+            targets: ["PayULoggerKit"]),
         .library(
             name: "PayUIndia-Networking",
             targets: ["PayUIndia-NetworkingTarget"]),
         .library(
-            name: "PayUIndia-UPICore", 
+            name: "PayUIndia-UPICore",
             targets: ["PayUIndia-UPICoreTarget"]),
         .library(
-            name: "PayUIndia-UPI", 
+            name: "PayUIndia-UPI",
             targets: ["PayUIndia-UPITarget"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-//        .package(name: "SocketIO", url: "https://github.com/ShubhGar/socket.io-client-swift.git", .exact("18.0.0"))
-
-        .package(name: "PayUIndia-PayUParams",url: "https://github.com/payu-intrepos/payu-params-iOS.git", from: "3.2.0"),
-
-        
+        .package(name: "PayUIndia-PayUParams",url: "https://github.com/payu-intrepos/payu-params-iOS.git", from: "3.2.1"),
     ],
     targets: [
-        .binaryTarget(name: "PayUIndia-Logger", path: "./Dependencies/PayULoggerKit.xcframework"),
-        .binaryTarget(name: "PayUIndia-Networking", path: "./Dependencies/PayUNetworkingKit.xcframework"),
-        .binaryTarget(name: "PayUIndia-UPICore", path: "./Dependencies/PayUUPICoreKit.xcframework"),
-        .binaryTarget(name: "PayUIndia-UPI", path: "./Dependencies/PayUUPIKit.xcframework"),
+        .binaryTarget(name: "PayULoggerKit", path: "./Dependencies/PayULoggerKit.xcframework"),
+        .binaryTarget(name: "PayUNetworkingKit", path: "./Dependencies/PayUNetworkingKit.xcframework"),
+        .binaryTarget(name: "PayUUPICoreKit", path: "./Dependencies/PayUUPICoreKit.xcframework"),
+        .binaryTarget(name: "PayUUPIKit", path: "./Dependencies/PayUUPIKit.xcframework"),
         .binaryTarget(name: "Starscream", path: "./Dependencies/Starscream.xcframework"),
         .binaryTarget(name: "SocketIO", path: "./Dependencies/SocketIO.xcframework"),
-            .target(
-                name: "PayUIndia-NetworkingTarget",
-                dependencies: [
-                    .target(name: "PayUIndia-Networking", condition: .when(platforms: [.iOS])),
-                    "PayUIndia-Logger"
-                  
-                ],
-                path: "Wrappers/PayUIndia-NetworkingWrapper"
-            ),
+        .target(
+            name: "PayUIndia-NetworkingTarget",
+            dependencies: [
+                "PayUNetworkingKit",
+                "PayULoggerKit"
+            ],
+            path: "Wrappers/PayUIndia-NetworkingWrapper"
+        ),
         .target(
             name: "PayUIndia-UPICoreTarget",
             dependencies: [
-                .target(name: "PayUIndia-UPICore", condition: .when(platforms: [.iOS])),
                 .product(name: "PayUIndia-PayUParams", package: "PayUIndia-PayUParams"),
                 "SocketIOTarget",
                 "PayUIndia-NetworkingTarget",
+                "PayUUPICoreKit"
             ],
             path: "Wrappers/PayUIndia-UPICoreWrapper"
         ),
         .target(
             name: "SocketIOTarget",
             dependencies: [
-                .target(name: "SocketIO", condition: .when(platforms: [.iOS])),
+                "SocketIO",
                 "Starscream"
             ],
             path: "Wrappers/SocketIOWrapper"
@@ -66,8 +61,8 @@ let package = Package(
         .target(
             name: "PayUIndia-UPITarget",
             dependencies: [
-                .target(name: "PayUIndia-UPI", condition: .when(platforms: [.iOS])),
-                "PayUIndia-UPICore"
+                "PayUUPIKit",
+                "PayUUPICoreKit"
             ],
             path: "Wrappers/PayUIndia-UPIWrapper"
         ),
