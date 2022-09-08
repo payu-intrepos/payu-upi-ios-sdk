79cd332939a421278d4adfb78ea475cbb116aea9
Shubham Garg <91garg.shubham@gmail.com>
2022-09-08 15:11:55 +0530

    Remove Socket IO dependency

--- PayUIndia-UPI-Release.sh
+++ PayUIndia-UPI-Release.sh
@@ -50,7 +50,7 @@ podTrunkPush() {
 }
 
 podName="PayUIndia-UPI"
-podVersion="6.0.1"
+podVersion="7.0.0"
 
 podSpec="${podName}.podspec"
 tag="${podName}_${podVersion}"
--- PayUIndia-UPICore-Release.sh
+++ PayUIndia-UPICore-Release.sh
@@ -50,7 +50,7 @@ podTrunkPush() {
 }
 
 podName="PayUIndia-UPICore"
-podVersion="6.3.1"
+podVersion="7.0.0"
 
 podSpec="${podName}.podspec"
 tag="${podName}_${podVersion}"
