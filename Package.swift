d0abef66c54ac87f2f0bc1eff446ac8b46d50205
mobilepayu <payumoneyapp@payumoney.com>
2025-04-10 16:05:41 +0530

    Supported OTM and Enforced Offer

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let VERSION_PARAM_KIT: PackageDescription.Version = "6.3.0"
+let VERSION_PARAM_KIT: PackageDescription.Version = "6.5.0"
 let VERSION_COMMON_UI: PackageDescription.Version = "2.1.0"
 
 let package = Package(
